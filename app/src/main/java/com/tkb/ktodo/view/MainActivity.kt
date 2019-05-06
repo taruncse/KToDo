78edcaec9442f9d38110e4d5f1a495c1fe7028d7
Tarun <tarun.pstu@gmail.com>
2019-05-06 17:52:29 +0600

    Added items on navigation drawer

--- app/src/main/java/com/tkb/ktodo/view/MainActivity.kt
+++ app/src/main/java/com/tkb/ktodo/view/MainActivity.kt
@@ -2,17 +2,15 @@ package com.tkb.ktodo.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import co.zsmb.materialdrawerkt.builders.accountHeader
 import co.zsmb.materialdrawerkt.builders.drawer
 import co.zsmb.materialdrawerkt.draweritems.badgeable.primaryItem
-import co.zsmb.materialdrawerkt.draweritems.badgeable.secondaryItem
 import co.zsmb.materialdrawerkt.draweritems.divider
 import co.zsmb.materialdrawerkt.draweritems.profile.profile
-import co.zsmb.materialdrawerkt.draweritems.profile.profileSetting
 import com.mikepenz.fontawesome_typeface_library.FontAwesome
-import com.mikepenz.google_material_typeface_library.GoogleMaterial
 import com.mikepenz.materialdrawer.AccountHeader
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.tkb.ktodo.R
 
 class MainActivity : AppCompatActivity() {
@@ -29,14 +27,25 @@ class MainActivity : AppCompatActivity() {
 
             primaryItem("Home") {
                 iicon = FontAwesome.Icon.faw_home
+                onClick { _ ->
+                    Log.d("DRAWER", "Click.")
+                    false
+                }
             }
             divider {}
             primaryItem("Users") {
                 iicon = FontAwesome.Icon.faw_home
+                onClick { _ ->
+                    Log.d("DRAWER", "Click.")
+                    false
+                }
             }
-            secondaryItem("Settings") {
+            primaryItem("Settings") {
                 iicon = FontAwesome.Icon.faw_home
-                selectable = false
+                onClick { _ ->
+                    Log.d("DRAWER", "Click.")
+                    false
+                }
             }
 
             headerResult = accountHeader {
@@ -48,27 +57,8 @@ class MainActivity : AppCompatActivity() {
                     iconUrl = "https://avatars3.githubusercontent.com/u/1476232?v=3&s=460"
                     identifier = 100
                 }
-                profile("Bernat Borras", "alorma@github.com") {
-                    iconUrl = "https://avatars3.githubusercontent.com/u/887462?v=3&s=460"
-                    identifier = 101
-                }
-
-                profileSetting("Manage Account") {
-                    iicon = GoogleMaterial.Icon.gmd_settings
-                    identifier = 100_001
-                }
-
                 onProfileChanged { _, profile, _ ->
-                    if (profile.identifier == 100_000L) {
-                        val size = headerResult.profiles.size
-                        val newProfile = ProfileDrawerItem()
-                            .withName("New Batman ${size - 1}")
-                            .withNameShown(true)
-                            .withEmail("batman${size - 1}@gmail.com")
-                            .withIcon(R.drawable.profile5)
-                            .withIdentifier(100L + size + 1L)
-                        headerResult.addProfile(newProfile, size - 2)
-                    }
+                    Toast.makeText(baseContext, profile.name.toString(), Toast.LENGTH_LONG).show()
                     false
                 }
             }
